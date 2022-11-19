d996cf597a44447f606a0013547d4391abe69734
Saugat pandey <saugat.pandey@festnepal.com>
2022-11-19 15:57:16 +0545

    created a portfolio

---
+++ src/.components/Projects.js
@@ -0,0 +1,93 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          }, 
+    ];
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>
+                            Projects
+                        </h2>
+                        <p>
+                            Lorem Ipsum
+                        </p>
+                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                    <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                                    </Nav.Item>
+                                    <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                                    </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            projects.map((project,index)=>{
+                                                return(
+                                                  <ProjectCard 
+                                                  key={index}
+                                                  {...project}
+                                                  />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                            Lorem Ipsum
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    lorem ipsum
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="bakground-image-right" src={colorSharp2}/>
+        </section>
+    )
+}
