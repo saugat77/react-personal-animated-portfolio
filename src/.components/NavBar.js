713f82bd7edc9e8cb3355b72703799226c41246d
saugat77 <saugatpandey4@gmail.com>
2023-12-10 13:22:38 -0500

    added logo and changed some project images

--- src/.components/NavBar.js
+++ src/.components/NavBar.js
@@ -1,57 +1,57 @@
-import { useState,useEffect } from "react";
-import { Navbar,Container, Nav } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import { useState, useEffect } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/img/saugat-high-resolution-logo-white-transparent.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/github-mark.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-export const NavBar = () =>{
-    const {activeLink, setActiveLink} = useState('home');
-    const {scrolled, setScrolled} = useState(0);
+export const NavBar = () => {
+  const { activeLink, setActiveLink } = useState('home');
+  const { scrolled, setScrolled } = useState(0);
 
-    useEffect(()=>{
-        const onScroll = () =>{
-            if(window.scrollY > 50){
-                setScrolled(true);
-            }
-            else{
-                setScrolled(false);
-            }
-        }
-        window.addEventListener("scroll",onScroll);
-        return()=>window.removeEventListener("scroll",onScroll);
-
-    },[])
-
-    const onUpdateActiveLink = (value) =>{
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      }
+      else {
+        setScrolled(false);
+      }
     }
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
 
-    return(
-        <Navbar expand="lg" className= {scrolled ? "Scrolled": ""} >
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggle-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link':'navbar-link'} onClick = {()=>onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link':'navbar-link'} onClick = {()=>onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link':'navbar-link'}onClick = {()=>onUpdateActiveLink('projects')}>Projects</Nav.Link>
-            </Nav>
-            <span className="navbar-text">
-                <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt=""/></a>
-                    <a href="#"><img src={navIcon2} alt=""/></a>
-                    <a href="#"><img src={navIcon3} alt=""/></a>
-                </div>
-                <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
-            </span>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "Scrolled" : ""} >
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggle-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/saugatpandey/"><img src={navIcon1} alt="" /></a>
+              <a href="https://github.com/saugat77"><img src={navIcon2} alt="" /></a>
+              <a href="https://www.instagram.com/saugatpandey4/"><img src={navIcon3} alt="" /></a>
+            </div>
+            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
--- src/.components/Projects.js
+++ src/.components/Projects.js
@@ -1,43 +1,43 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/ecommerce.jpg";
+import projImg2 from "../assets/img/gbho.png";
+import projImg3 from "../assets/img/kantipur.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 
 export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Ecommerce Website",
+      description: "Design & Developed For Final Year Project Using Laravel",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "GBHO",
+      description: "Design & Developed For a US based Company With Payment System and Membership Registration Using Laravel",
       imgUrl: projImg2,
     },
     {
       title: "Business Startup",
-      description: "Design & Development",
+      description: "Design & Developed For a Nepali Foreign Employment Company which has unique feature i.e. Image Recognization Using Laravel Vue and Tessaract",
       imgUrl: projImg3,
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg2,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
   ];
 
   return (
@@ -45,43 +45,85 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp">
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
+            <h2>Projects</h2>
+            <p></p>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Images</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Ecommerce Web application</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">GBHO</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="fourth">Kantipur Overseas</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content id="slideInUp">
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {
+                      projects.map((project, index) => {
+                        return (
+                          <ProjectCard
+                            key={index}
+                            {...project}
+                          />
+                        )
+                      })
+                    }
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <p>
+                    <b>Ecommerce Website:</b>
+                    In my final year project, I developed a complete ecommerce website using Laravel for the backend. The platform caters to two
+                    main user roles: sellers, who upload products and manage categories and users, and customers, who browse, add items to their
+                    cart, and can create wishlists. The website follows the typical ecommerce workflow: users can register/login, add items to their
+                    cart, and proceed to checkout. Payment options include cash on delivery or using PayPal or Khalti. It was a solo project aimed at
+                    showcasing a Business-to-Customer (B2C) ecommerce model. <br />
+
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <p>  <b>GBHO Project:</b>
+                    At FestNepal, I actively contributed to a multifaceted project aimed at creating a web platform for booking priests, managing
+                    events, running an ecommerce website, and overseeing a school management system. This US-based project was initiated by a
+                    Hindu organization striving to establish its ecosystem through a comprehensive web application.
+                    The project's workflow allowed users to purchase membership packs via PayPal for registration. Alternatively, individuals could
+                    register as priests or volunteers but they had to get approval from the admin team. I took complete ownership of the PayPal
+                    integration and developed user roles and permissions for registered members. Managing authorization processes was a key
+                    aspect of my role.
+                    Beyond this, I played pivotal roles in creating other systems such as library management and assignment tracking. I specifically
+                    developed the event and priest booking systems, enabling members to book priests based on the time availability of the priest.
+                    Registration for priest booking necessitated PayPal payments, aligning with the availability of the priests. And I even handled the
+                    jobs that can deliver the email if the membership renewable time period has arrived and all the website email handling was
+                    managed by me.
+                    This project was not just fulfilling but also presented numerous challenges that expanded my learning horizons. Despite not
+                    having an internship, I gained substantial knowledge, especially in handling CPanel hosting and overseeing database
+                    management for both live and test servers. This experience provided insights into deployment processes and the functioning of
+                    an IT company. Though the journey ended after a year, it was an enriching period where I acquired practical skills and insights
+                    into various aspects of web development and IT operations. <br /></p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="fourth">
+                  <p>
+                    <b>Kantipur Overseas Website:</b>
+                    Kantipur Overseas is the one of the oldest agency serving about 25 years in the foreign employment sector. Working on Kantipur
+                    Overseas, web application using Laravel and Vue.js is an exciting opportunity. I've integrated a unique text recognition feature
+                    specifically designed to identify crucial documents like police reports, which sets our project apart in the foreign employment
+                    field. Our application offers various features, including displaying job demands, detailed descriptions, user authentication,
+                    director appointment scheduling, and a specialized resume builder that uses government-issued police report images.
+                    My enthusiasm for technology and ability to adapt are evident, showcased by my leadership experience at FestNepal. I'm a quick
+                    learner and a team player, qualities that significantly contribute to project success. If there are new opportunities available, my
+                    skill set and eagerness to grow could be valuable assets to any team. I'm committed to embracing and conquering the exciting
+                    challenges in the tech world!
+                  </p>
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </Col>
         </Row>
       </Container>
